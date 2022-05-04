8b19a716cec5b6be09cbbdee39a4c79decc840c4
lvlei <2550515155@qq.com>
2022-05-04 18:23:43 +0800

    增加库存和销量分析图表

--- vision/src/components/StockCom.vue
+++ vision/src/components/StockCom.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="com-container">StockCom
+  <div class="com-container">
     <div class="com-chart" ref="stock-ref"></div>
   </div>
 </template>
@@ -10,7 +10,9 @@ export default {
   data () {
     return {
       jsonData: null,
-      chartInstance: null
+      chartInstance: null,
+      currentIndex: 0,
+      timerId: null
     }
   },
   computed: {},
@@ -22,28 +24,160 @@ export default {
   },
   destroyed () {
     window.removeEventListener('resize', this.screenAdapter)
+    if (this.timerId) {
+      clearInterval(this.timerId)
+    }
   },
   methods: {
     initChart () {
       this.chartInstance = this.$echarts.init(this.$refs['stock-ref'], 'chalk')
-      const initOption = {}
+      const initOption = {
+        title: {
+          text: '▋ 库存和销量分析',
+          left: 20,
+          top: 20
+        }
+      }
       this.chartInstance.setOption(initOption)
+      this.chartInstance.on('mouseover', () => {
+        clearInterval(this.timerId)
+      })
+      this.chartInstance.on('mouseout', () => {
+        this.startInterval()
+      })
     },
     async getData () {
       await this.$axios.get('stock').then(res => {
         this.jsonData = res.data
-        // console.log(this.jsonData)
         this.updateChart()
+        this.startInterval()
       })
     },
     updateChart () {
-      const dataOption = {}
+      const centerArr = [
+        ['18%', '40%'],
+        ['50%', '40%'],
+        ['82%', '40%'],
+        ['34%', '75%'],
+        ['66%', '75%']
+      ]
+      const colorArr = [
+        ['#4FF778', '#0BA82C'],
+        ['#E5DD45', '#E8B11C'],
+        ['#E8821C', '#E55445'],
+        ['#5052EE', '#AB6EE5'],
+        ['#23E5E5', '#2E72BF']
+      ]
+      const start = this.currentIndex * 5
+      const end = (this.currentIndex + 1) * 5
+      const showData = this.jsonData.slice(start, end)
+      const seriesArr = showData.map((item, index) => {
+        return {
+          type: 'pie',
+          // radius: [110, 100],
+          center: centerArr[index],
+          hoverAnimation: false, // 关闭鼠标移入图表时的动画效果
+          labelLine: {
+            show: false // 隐藏指示线
+          },
+          label: {
+            position: 'center',
+            color: colorArr[index][0]
+          },
+          data: [
+            {
+              // name: item.name + '\n' + item.sales,
+              value: item.sales,
+              itemStyle: {
+                color: '#333843'
+              }
+            },
+            {
+              name: item.name + '\n' + item.stock,
+              value: item.stock,
+              itemStyle: {
+                color: new this.$echarts.graphic.LinearGradient(0, 1, 0, 0, [
+                  {
+                    offset: 0,
+                    color: colorArr[index][0]
+                  },
+                  {
+                    offset: 1,
+                    color: colorArr[index][1]
+                  }
+                ])
+              }
+            }
+          ]
+        }
+      })
+      const dataOption = {
+        series: seriesArr
+      }
       this.chartInstance.setOption(dataOption)
     },
     screenAdapter () {
-      const adapterOption = {}
+      const titleFontSize = this.$refs['stock-ref'].offsetWidth / 100 * 4.5
+      const innerRadius = titleFontSize * 2
+      const outterRadius = innerRadius * 1.125
+      const adapterOption = {
+        title: {
+          textStyle: {
+            fontSize: titleFontSize
+          }
+        },
+        series: [
+          {
+            type: 'pie',
+            radius: [outterRadius, innerRadius],
+            label: {
+              fontSize: titleFontSize / 2
+            }
+          },
+          {
+            type: 'pie',
+            radius: [outterRadius, innerRadius],
+            label: {
+              fontSize: titleFontSize / 2
+            }
+          },
+          {
+            type: 'pie',
+            radius: [outterRadius, innerRadius],
+            label: {
+              fontSize: titleFontSize / 2
+            }
+          },
+          {
+            type: 'pie',
+            radius: [outterRadius, innerRadius],
+            label: {
+              fontSize: titleFontSize / 2
+            }
+          },
+          {
+            type: 'pie',
+            radius: [outterRadius, innerRadius],
+            label: {
+              fontSize: titleFontSize / 2
+            }
+          }
+        ]
+      }
       this.chartInstance.setOption(adapterOption)
       this.chartInstance.resize()
+    },
+    startInterval () {
+      if (this.timerId) {
+        clearInterval(this.timerId)
+      }
+      this.timerId = setInterval(() => {
+        this.currentIndex++
+        if (this.currentIndex > 1) {
+          this.currentIndex = 0
+        }
+        this.updateChart()
+      }, 5000)
     }
   }
 }
